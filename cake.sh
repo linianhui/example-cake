e81e0b93dfb744296cdbde5ad9b4ab13767a811c
linianhui <lnhcode@outlook.com>
2018-08-24 10:32:38 +0800

    Update cake.sh

--- cake.sh
+++ cake.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
-# Define default arguments.
 SCRIPT="0-build/build.cake"
-CAKE_ARGUMENTS=()
-
-# Parse arguments.
-for i in "$@"; do
-    case $1 in
-        -s|--script) SCRIPT="$2"; shift ;;
-        --) shift; CAKE_ARGUMENTS+=("$@"); break ;;
-        *) CAKE_ARGUMENTS+=("$1") ;;
-    esac
-    shift
-done
 
 # Install  cake.tool
 dotnet tool install --global cake.tool --version 0.30.0
 
 # Start Cake
-dotnet cake $SCRIPT "${CAKE_ARGUMENTS[@]}"
+
+echo "\033[32mdotnet cake $SCRIPT $@"
+
+dotnet cake $SCRIPT "$@"
