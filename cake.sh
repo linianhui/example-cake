13bfed10de60c448510c109eaa10d830b0fafe06
linianhui <lnhcode@outlook.com>
2018-08-24 10:53:29 +0800

    Update cake.sh

--- cake.sh
+++ cake.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-SCRIPT="0-build/build.cake"
+SCRIPT='0-build/build.cake'
+CAKE_VERSION='0.30.0'
 
 # Install  cake.tool
-dotnet tool install --global cake.tool --version 0.30.0
+dotnet tool install --global cake.tool --version $CAKE_VERSION
 
 # Start Cake
+CAKE_ARGS="$SCRIPT -verbosity=verbose"
 
-echo "\033[32mdotnet cake $SCRIPT $@"
+echo "\033[32mdotnet cake $CAKE_ARGS $@"
 
-dotnet cake $SCRIPT "$@"
+dotnet cake $CAKE_ARGS "$@"
