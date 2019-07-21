ae16d01970a26d3816b690bd93d53601e2f672c8
lnh <lnhcode@outlook.com>
2019-07-21 12:11:24 +0800

    Update cake to 0.34.1

--- cake.ps1
+++ cake.ps1
@@ -1,5 +1,5 @@
 [string]$SCRIPT       = '0-build/build.cake'
-[string]$CAKE_VERSION = '0.33.0'
+[string]$CAKE_VERSION = '0.34.1'
 
 # nuget server config
 $ENV:NUGET_REPOSITORY_API_URL = "http://nuget-server.test/nuget"
