5ad166081743f9136680bedf8b2e066f1e788e65
lnh <lnhcode@outlook.com>
2019-12-08 12:40:48 +0800

    Update cake.ps1

--- cake.ps1
+++ cake.ps1
@@ -1,12 +1,13 @@
-[string]$CAKE_ARGS = "0-build/build.cake -verbosity=diagnostic"
+[string]$CAKE_SCRIPT_FILE = '0-build/build.cake'
+[string]$CAKE_ARGS = '-verbosity=diagnostic'
 
-$ENV:NUGET_REPOSITORY_API_URL = "http://nuget-server.test/nuget"
-$ENV:NUGET_REPOSITORY_API_KEY = "123456"
+$ENV:NUGET_REPOSITORY_API_URL = 'http://nuget-server.test/nuget'
+$ENV:NUGET_REPOSITORY_API_KEY = '123456'
 
 dotnet --info
 
 dotnet tool restore
 
-Write-Host "dotnet cake $CAKE_ARGS $ARGS" -ForegroundColor GREEN
+Write-Host "dotnet cake $CAKE_SCRIPT_FILE $CAKE_ARGS $ARGS" -ForegroundColor GREEN
 
-dotnet cake $CAKE_ARGS $ARGS
+dotnet cake $CAKE_SCRIPT_FILE $CAKE_ARGS $ARGS
